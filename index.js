7c69b80e7c8354614c2394294242acdf4c38c46e
prajat <rajat.singh1084@gmail.com>
2018-07-03 12:29:29 +0530

    editing indes js

--- index.js
+++ index.js
@@ -126,49 +126,9 @@ app.get('/url/:encoded_id', function (req, res) {
 
 //Testing server request
 app.get('/hello/', function (req, res) {
-    var country, region, city, timezone;
-    var deviceType = req.device.type;
-
-    var agent = useragent.parse(req.headers['user-agent']);
-    const IP = req.clientIp;
-    var os = agent.os.toString();
-
-    request.get({
-        url: "http://ip-api.com/json",
-        json: true,
-        
-    }, (error, res, data) => {
-        if (error) {
-            console.log('Error:', error);
-            res.json({
-                success: false,
-                err: 'Problem in Geo Location API!!!'
-            });
-        } else if (res.statusCode !== 200) {
-            console.log('Status:', res.statusCode);
-            res.json({
-                success: false,
-                err: 'No data Found for this IP!!!!'
-            });
-        } else {
-            if (data.status == 'fail') {
-                res.json({
-                    success: false,
-                    err: "Invalid IP " + data.query
-                });
-            } else {
-                country = data.country;
-                timezone = data.timezone;
-                
-                city = data.city;
-                region = data.regionName;
-                console.log(country, timezone, city, region);
-                           
-            }
-        }
-    });
     
-    var data = { ip :req.clientIp,deviceType,os,timestamp:timezone,country, region, city};
+    
+    var data = { ip :req.clientIp,deviceType,os,country, region, city};
 
 request.post({
     headers: {'content-type': 'application/json'},
